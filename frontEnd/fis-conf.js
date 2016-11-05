f80fe95785b2c5054dc6f00f0c6444b846c26da0
xxsgithub <wxxzhuang@gmail.com>
2016-11-06 05:55:44 +0800

    重新调整了文件的位置，加了后端的文件，修复已知bug，完成构建工具的配置

---
+++ frontEnd/fis-conf.js
@@ -0,0 +1,121 @@
+/**
+ * Created by XX on 2016/11/4.
+ */
+
+fis.set('project.ignore', [
+    'bower_components/**',
+    'widget/**',
+    'node_modules',
+    '/.gitignore',
+    '/*.json',
+    '/*.md',
+    '/bower_components/bootstrap/dist/css/*.map'
+]);
+
+fis.match('::packager', {
+    postpackager: fis.plugin('loader')
+});
+
+//      对app.js的处理
+fis.match('/widget/*.js', {
+    url: '/javascripts/app.js',
+    optimizer: fis.plugin('uglify-js'),
+    release: '/public/javascripts/app.js',
+    packTo:'/public/javascripts/app.js'
+});
+fis.match('bower_components/jquery/dist/jquery.min.js', {
+    url: '/javascripts/app.js',
+    release: '/public/javascripts/app.js',
+    packTo: '/public/javascripts/app.js'
+});
+fis.match('bower_components/bootstrap/dist/js/bootstrap.min.js', {
+    url: '/javascripts/app.js',
+    release: '/public/javascripts/app.js',
+    packTo: '/public/javascripts/app.js'
+});
+
+//       对ie.jd的处理
+fis.match('bower_components/html5shiv/dist/html5shiv.min.js', {
+    url: '/javascripts/ie.js',
+    release: '/public/javascripts/ie.js',
+    packTo: '/public/javascripts/ie.js'
+});
+fis.match('bower_components/respond/dest/respond.min.js', {
+    url: '/javascripts/ie.js',
+    release: '/public/javascripts/ie.js',
+    packTo: '/public/javascripts/ie.js'
+});
+
+//         对CSS的处理
+fis.match('/widget/*.css', {
+    url: '/stylesheets/app.css',
+    optimizer: fis.plugin('clean-css'),
+    preprocessor: fis.plugin('autoprefixer', {
+        "browsers": ["Android >= 2.1", "iOS >= 4", "ie >= 8", "firefox >= 15"],
+        "cascade": true
+    }),
+    packTo: '/public/stylesheets/app.css',
+    release: '/public/stylesheets/app.css'
+});
+fis.match('bower_components/bootstrap/dist/css/bootstrap.min.css', {
+    url: '/stylesheets/app.css',
+    release: '/public/stylesheets/app.css',
+    packTo: '/public/stylesheets/app.css'
+});
+fis.match('bower_components/font-awesome/css/font-awesome.min.css', {
+    url: '/stylesheets/app.css',
+    release: '/public/stylesheets/app.css',
+    packTo: '/public/stylesheets/app.css'
+});
+
+//     对图片的处理
+fis.match('/images/*.png', {
+    url: '$0',
+    optimizer: fis.plugin('png-compressor'),
+    release: '/public/$0'
+});
+fis.match('/images/*.{jpg,ico}', {
+    url: '$0',
+    release: '/public/$0'
+});
+fis.match('/images/*.tif', {
+    release: false
+});
+
+//    对html的处理
+fis.match('*.html', {
+    optimizer: fis.plugin('html-compress'),
+    release: '/public/html/$0',
+    useCache: false
+});
+
+//     忽略fis-conf.js
+fis.match('/*.js', {
+    release: false
+});
+
+//      将fonts和bootstrap需要的map文件位置移动
+fis.match('bower_components/bootstrap/dist/fonts/*', {
+    url: '/fonts$0',
+    release: '/public/fonts/$0'
+});
+fis.match('bower_components/font-awesome/fonts/*', {
+    url: '/fonts$0',
+    release: '/public/fonts/$0'
+});
+fis.match('bower_components/bootstrap/dist/css/bootstrap.min.css.map', {
+    release: '/public/stylesheets/$0'
+});
+
+
+//     修改html里面的js和css的路径
+fis.match('public/javascripts/app.js', {
+    url: '/javascripts/app.js'
+});
+fis.match('public/javascripts/ie.js', {
+    url: '/javascripts/ie.js'
+});
+fis.match('public/stylesheets/app.css', {
+    url: '/stylesheets/app.css'
+});
+
---
+++ sys/routes/index.js
@@ -0,0 +1,187 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var dirname = path.dirname(__dirname);
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.sendFile(dirname + '/public/html/index.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/index.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/index.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_foreword.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_foreword.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_introduction.html', function (req, res, next) {
+    res.sendFile(dirname + '/public/html/guide_introduction.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_clienterror.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_clienterror.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_join.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_join.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_newclient.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_newclient.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_paper.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_paper.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_pcPrepare1.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_pcPrepare1.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_pcprepare2.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_pcprepare2.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_pcset1.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_pcset1.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_pcset2.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_pcset2.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_pcset3.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_pcset3.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_phonePrepare.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_phonePrepare.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_phonerepair.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_phonerepair.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_phoneset.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_phoneset.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_pppoeerror.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_pppoeerror.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_repair.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_repair.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/guide_wechat.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/guide_wechat.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+router.get('/join.html', function (req, res, next) {
+    res.sendFile(dirname +'/public/html/join.html', function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+module.exports = router;
