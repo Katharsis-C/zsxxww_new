71bda422b351ba1750ed0963900f44d63ca04b62
xxsgithub <wxxzhuang@gmail.com>
2016-11-27 17:51:00 +0800

    完成operator的部分实现，因为某一页是要删除的，调整了html和css

---
+++ frontEnd/widget/operator.js
@@ -0,0 +1,51 @@
+/**
+ * Created by XX on 2016/11/27.
+ */
+
+function pos(type, step, ele) {
+    if (_.isNumber(step)) {
+        ele.css(type, _.add(_.parseInt(ele.css(type)), step) + 'px');
+    }
+}
+
+function inRange(number, start, range) {
+    range = Math.abs(range);
+    return _.inRange(number, start, start + range) || _.inRange(number, start - range, start);
+}
+
+function posUntil(type, step, end, timeout, ele) {
+    if (_.isNumber(step) &&
+        _.isNumber(end) &&
+        Math.abs(end - parseInt(ele.css(type))) >
+        Math.abs(end - _.add(_.parseInt(ele.css(type)), step))) {
+
+        var operate = operator[type](step);
+
+        setTimeout(function () {
+            if (inRange(parseInt(ele.css(type)), end, step)) {
+                ele.css(type, end + 'px');
+                clearTimeout(arguments.callee, timeout);
+            } else {
+                operate(ele);
+                setTimeout(arguments.callee, timeout);
+            }
+        }, timeout);
+
+    } else if (_.isNumber(end)) {
+        ele.css(type, end + 'px');
+    }
+}
+
+var curried_pos = _.curry(pos);
+var curried_posUntil = _.curry(posUntil);
+
+var operator = {
+    left: curried_pos('left'),
+    right: curried_pos('right'),
+    top: curried_pos('top'),
+    bottom: curried_pos('bottom'),
+    leftUntil: curried_posUntil('left'),
+    rightUntil: curried_posUntil('right'),
+    topUntil: curried_posUntil('top'),
+    bottomUntil: curried_posUntil('bottom')
+};
