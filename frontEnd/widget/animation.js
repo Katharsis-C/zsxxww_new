e48fd52c7c9f46106e971de77de9d5eb12293e41
xxsgithub <wxxzhuang@gmail.com>
2016-11-28 23:32:39 +0800

    动画连贯 get ，TODO：修改参数完善动画

--- frontEnd/widget/animation.js
+++ frontEnd/widget/animation.js
@@ -1,5 +1,34 @@
 /**
- * Created by XX on 2016/11/24.
+ * Created by XX on 2016/11/27.
  */
 
+
+
+function Animation() {
+
+}
+
+//    初始化ppt动画
+Animation.prototype.pptInit = function () {
+    var nowPage = $('.page');
+    nowPage.on('animationEnd webkitAnimationEnd oAnimationEnd', function (event) {
+        var nextPage = $(event.target).next();
+        if (nextPage.hasClass('page')){
+            nextPage.css('animation-play-state','running').addClass('active');
+        }
+    })
+};
+
+//    开始ppt动画
+Animation.prototype.pptStart = function () {
+    $('#page1').css('animation-play-state','running').addClass('active');
+};
+
+var animation = new Animation();
+
+animation.pptInit();
+
+$(document).ready(function () {
+    animation.pptStart();
+});
 
