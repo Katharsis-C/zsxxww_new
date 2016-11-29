22c1907723675e133658e410ffb4a4fcf69f91b2
xxsgithub <wxxzhuang@gmail.com>
2016-11-29 11:03:03 +0800

    完成对page1-14的调整，page15-page18有个蜜汁bug，TODO:继续调整

--- frontEnd/widget/animation.js
+++ frontEnd/widget/animation.js
@@ -13,15 +13,15 @@ Animation.prototype.pptInit = function () {
     var nowPage = $('.page');
     nowPage.on('animationEnd webkitAnimationEnd oAnimationEnd', function (event) {
         var nextPage = $(event.target).next();
-        if (nextPage.hasClass('page')){
-            nextPage.css('animation-play-state','running').addClass('active');
+        if (nextPage.hasClass('page')) {
+            nextPage.css('animation-play-state', 'running').addClass('active');
         }
-    })
+    });
 };
 
 //    开始ppt动画
 Animation.prototype.pptStart = function () {
-    $('#page1').css('animation-play-state','running').addClass('active');
+    $('#page15').css('animation-play-state', 'running').addClass('active');
 };
 
 var animation = new Animation();
